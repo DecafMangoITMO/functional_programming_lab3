38e1e9d3549c01b7e72a21133ff76635991376b7
DecafMango <DecafMangoITMO@yandex.ru>
2024-10-19 15:08:23 +0300

    initial commit

--- test/functional_programming_lab3_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FunctionalProgrammingLab3Test do
-  use ExUnit.Case
-  doctest FunctionalProgrammingLab3
-
-  test "greets the world" do
-    assert FunctionalProgrammingLab3.hello() == :world
-  end
-end
